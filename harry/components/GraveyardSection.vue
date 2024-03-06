22c1688e8350916265a8dae8627a196e1034bcde
Cecilio Morales <cecilio.morales@leniolabs.com>
2024-03-06 17:55:08 -0500

    Graveyard fix validation

--- harry/components/GraveyardSection.vue
+++ harry/components/GraveyardSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white shadow rounded-lg p-4 mt-4" v-if="graveyardInfo != null">
+  <div class="bg-white shadow rounded-lg p-4 mt-4" v-if="isValidGraveyardInfo">
     <h2 class="text-lg font-bold mb-2">Graveyard Directions</h2>
     <div v-if="graveyardInfo!.place_id" ref="map_element" class="border-b border-t border-r border-gray-200 h-40 w-full rounded-t-md" id="map"></div>
     <div class="border-b border-l border-r mt-2 border-gray-200 pl-4 pb-4 pr-4 rounded-b-md">
@@ -20,6 +20,7 @@
 <script setup lang="ts">
 const profileStore = useProfileStore();
 const graveyardInfo = computed(() => profileStore.profile.grave_yard_info);
+const isValidGraveyardInfo = computed(() => profileStore.profile.grave_yard_info?.name);
 if (process.server && profileStore.profile.grave_yard_info?.place_id !== null) {
   useHead({ script: [{ src: `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_PLACES_KEY}&v=weekly`, defer: true }] });
 }
