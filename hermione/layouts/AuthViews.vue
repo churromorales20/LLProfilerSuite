e82d7426a2c2a61eb400f0f18b2e1e65355f461e
Cecilio Morales <cecilio.morales@leniolabs.com>
2024-03-08 09:20:44 -0500

    Bg color auth hermione

--- hermione/layouts/AuthViews.vue
+++ hermione/layouts/AuthViews.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen flex flex-col justify-center py-12 px-4 sm:px-6 lg:px-8">
+  <div class="bg-white min-h-screen flex flex-col justify-center py-12 px-4 sm:px-6 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-md">
       <img style="height:170px;" class="mx-auto w-auto" src="/logo.png" alt="Logo">
     </div>
